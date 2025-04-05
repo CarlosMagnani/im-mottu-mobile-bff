a173272a3dff92fa0cb0a44ceac8ec01b7161178
Carlos Magnani <carlosdsmagnani@gmail.com>
2025-04-05 00:06:05 -0300

    refactor: move global exception filter to a dedicated directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SwaggerModule } from './shared/presentation/swagger/swagger.module';
-import { GlobalExceptionFilter } from './shared/infrastructure/filters/global-exception.filter';
+import { GlobalExceptionFilter } from './shared/infrastructure/exception-filters/global-exception.filter';
 import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 @Module({
